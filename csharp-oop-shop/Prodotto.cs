0ea295beaedb1ca03dc5f538050888d6c576d9c6
colombovf <colombovf@gmail.com>
2022-11-30 09:37:54 +0100

    Ho scordato di pushare, perdono [Fine esercizio]

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -19,22 +19,84 @@ namespace csharp_oop_shop
         public Prodotto() { }
 
         //Costruttori con parametri
-        public Prodotto(int codice, string name, string description, int price, int iva) { 
-            this.codice = codice;
+        public Prodotto(string name, string description, int price)
+        {
+            this.codice = setCodice();
             this.nome = name;
             this.descrizione = description;
             this.prezzo = price;
-            this.iva = iva;           
+            this.iva = getIva();
         }
         //Getter
         public int getCodice()
         {
-            return codice; 
+            return codice;
         }
 
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public string getDescrizione()
+        {
+            return descrizione;
+        }
+
+        public int getPrezzo()
+        {
+            return prezzo - iva;
+        }
+
+        public int getPrezzoConIva()
+        {
+            return prezzo;
+        }
+
+        public int getIva()
+        {
+            
+            return CalcoloIva(prezzo);
+        }
+
+        //Setter
+
+        public int setCodice()
+        {
+            Random r = new Random();
+           return  r.Next(0, 9999999);
+        }
         public void setNome(string name)
         {
             nome = name;
+        }
+
+        public void setDescrizione(string description)
+        {
+            descrizione = description;
+        }
+
+        public void setPrezzo(int price)
+        {
+            this.prezzo = price;
+            iva = CalcoloIva(price);
+        }
+
+        public void setPrezzoConIva(int price)
+        {
+            iva = CalcoloIva(price);
+            prezzo = CalcoloPrezzoConIva(price);
+        }
+        //Funzioni
+
+        int CalcoloIva(int price)
+        {
+            return (price * 20) / 100;
+        }
+
+        int CalcoloPrezzoConIva(int price)
+        {
+            return price - (price * 20 / 100);
         }
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,11 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //creare la classe Prodotto che gestisce i prodotti del vostro shop.
 using csharp_oop_shop;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Xml.Linq;
 
 
-Prodotto p1= new Prodotto(789, "Candela", "Candela profumata", 55, 11);
+Prodotto p1= new Prodotto("Candela", "Candela Profumata", 20);
+Prodotto p2 = new Prodotto("Peso", "Strumento per allenare i bicipiti", 37);
+Prodotto p3 = new Prodotto("Bilancia", "Strumento per misurare il peso", 93);
+Prodotto p4 = new Prodotto("Vetril", "Prodotto per pulire i vetri", 4);
+Prodotto p5 = new Prodotto("Pistola", "Arma da fuoco", 870);
 
 
-Console.WriteLine(p1.getCodice());
+
+
+Console.WriteLine("Prodotto 1: " + p1.getNome());
+Console.WriteLine("Codice prodotto: " + p1.getCodice());
+Console.WriteLine("Descrizione: " + p1.getDescrizione());
+Console.WriteLine("Prezzo iva compresa: " + p1.getPrezzoConIva());
+Console.WriteLine("Iva: " + p1.getIva());
+Console.WriteLine("Prezzo senza iva: " + p1.getPrezzo());
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Prodotto 2: " + p2.getNome());
+Console.WriteLine("Codice prodotto: " + p2.getCodice());
+Console.WriteLine("Descrizione: " + p2.getDescrizione());
+Console.WriteLine("Prezzo iva compresa: " + p2.getPrezzoConIva());
+Console.WriteLine("Iva: " + p2.getIva());
+Console.WriteLine("Prezzo senza iva: " + p2.getPrezzo());
+
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Prodotto 3: " + p3.getNome());
+Console.WriteLine("Codice prodotto: " + p3.getCodice());
+Console.WriteLine("Descrizione: " + p3.getDescrizione());
+Console.WriteLine("Prezzo iva compresa: " + p3.getPrezzoConIva());
+Console.WriteLine("Iva: " + p3.getIva());
+Console.WriteLine("Prezzo senza iva: " + p3.getPrezzo());
+
+Console.WriteLine();
+Console.WriteLine();
+
+
+Console.WriteLine("Prodotto 4: " + p4.getNome());
+Console.WriteLine("Codice prodotto: " + p4.getCodice());
+Console.WriteLine("Descrizione: " + p4.getDescrizione());
+Console.WriteLine("Prezzo iva compresa: " + p4.getPrezzoConIva());
+Console.WriteLine("Iva: " + p4.getIva());
+Console.WriteLine("Prezzo senza iva: " + p4.getPrezzo());
